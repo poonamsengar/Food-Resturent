4c9eca624b73dcee12ceda6b0d45c5f0437d0538
Poonam Sengar <sengarpoonam2699@gmail.com>
2023-07-11 23:46:03 +0530

    main

--- scripts/Order.js
+++ scripts/Order.js
@@ -10,14 +10,15 @@ document.getElementById("Order").addEventListener("submit", (e) => {
   // OrderPlacesNull.append(orderName,orderEmail,orderNum,foodname,textArea);
 
   console.log(orderName.value);
-  const rightCLick = document.getElementById("rightCLick");
-      
-    
+  
+  const rightCLick = document.getElementById("rightCLick");  
+  
       if((orderName.value=="") && (orderEmail.value=="") && (orderNum.value=="") && (foodname.value=="") && (textArea.value=="")){
         alert("please Insert Your Information");
       }else{
         rightCLick.style.display = "block";
         document.getElementById("main").style.filter = "blur(6px)";
+
   
         document.getElementById("croshOrder").addEventListener("click",()=>{
           console.log("click");
